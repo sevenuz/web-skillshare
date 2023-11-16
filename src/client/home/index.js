0fa351e4f17de14b51deacd9986ee5af43d692fa
sevenuz <julius@athenstaedt.net>
2023-11-16 12:49:39 +0100

    sidebar with birds

--- src/client/home/index.js
+++ src/client/home/index.js
@@ -17,4 +17,27 @@ async function load_subtitle() {
 		});
 }
 
+let show = false;
+$("#bird").click(() => {
+	var pigeon = $("#pigeon");
+	pigeon.css({
+		display: show? "none": "block"
+	});
+	show = !show;
+});
+
+$("body").mousemove((event) => {
+	var eye = $("#eye");
+  var x = (eye.offset().left) + (eye.width() / 2);
+  var y = (eye.offset().top) + (eye.height() / 2);
+  var rad = Math.atan2(event.pageX - x, event.pageY - y);
+  var rot = (rad * (180 / Math.PI) * -1) + 180;
+  eye.css({
+    '-webkit-transform': 'rotate(' + rot + 'deg)',
+    '-moz-transform': 'rotate(' + rot + 'deg)',
+    '-ms-transform': 'rotate(' + rot + 'deg)',
+    'transform': 'rotate(' + rot + 'deg)'
+  });
+});
+
 $("#contacter").on("click", load_subtitle);
